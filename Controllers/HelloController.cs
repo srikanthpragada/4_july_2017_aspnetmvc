64561e26af2427f5ab505eb78c8fbe37bdddaa32
srikanthpragada <srikanthpragada@gmail.com>
2017-07-28 10:05:11 +0530

    28-JULY

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,44 @@
+﻿using mvcdemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemo.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            ViewBag.Today = DateTime.Now;
+            return View();
+        }
+
+        public ActionResult Info()
+        {
+           var person = new Person { Name = "Scott", Email = "scott@microsoft.com" };
+           return View(person);
+        }
+
+
+        public ActionResult Wish(string name)
+        {
+            string message = "Good Evening";
+
+            if (name == null)
+                name = "Mr. Unknown";
+
+            int hour = DateTime.Now.Hour;
+            if (hour < 12)
+                message = "Good Morning";
+            else
+                if (hour < 17)
+                message = "Good Afternoon";
+
+            ViewBag.Message = message + ", " + name;
+            return View();
+        }
+    }
+}
