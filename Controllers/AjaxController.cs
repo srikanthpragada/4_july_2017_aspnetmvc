2c1b868e7f21342d669f0e32ed13391b04f1bbdb
srikanthpragada <srikanthpragada@gmail.com>
2017-08-02 10:01:43 +0530

    2-AUG

---
+++ Controllers/AjaxController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemo.Controllers
+{
+    public class AjaxController : Controller
+    {
+        // GET: Ajax
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Now()
+        {
+            return DateTime.Now.ToString(); 
+        }
+
+        public string UserExists(string uname)
+        {
+            List<String> users = new List<string> { "Abc", "Bbc", "Xyz", "Pqr" };
+
+            // check whether uname is already present 
+            if (users.Contains(uname))
+                return "yes";
+            else
+                return "no";
+        }
+    }
+}
