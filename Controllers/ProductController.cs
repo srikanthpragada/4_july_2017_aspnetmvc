bb00500b6fa770c3d2eeb89b26a241d05bbd4455
srikanthpragada <srikanthpragada@gmail.com>
2017-08-03 10:00:31 +0530

    3-AUG

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -8,28 +8,29 @@ using System.Web.Mvc;
 
 namespace mvcdemo.Controllers
 {
+    [Authorize]
     public class ProductController : Controller
     {
         [OutputCache(Duration = 60)]
-        public ActionResult Index(int price)
+        public ActionResult Index(int price = 0 )
         {
-            ViewBag.CreatedOn = DateTime.Now.ToLongTimeString(); 
+            ViewBag.CreatedOn = DateTime.Now.ToLongTimeString();
             List<Product> prods = new List<Product>();
             using (SqlConnection con = new SqlConnection(
                 @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=msdb;Integrated Security=True"))
             {
                 con.Open();
 
-                SqlCommand cmd = new SqlCommand("select * from products where price >=" + price, con);
+                SqlCommand cmd = new SqlCommand("select * from products", con);
                 SqlDataReader dr = cmd.ExecuteReader();
-             
+
                 while (dr.Read())
                 {
                     prods.Add(new Product
-                       {
-                         ProdName = dr["prodname"].ToString(),
-                         Price = Double.Parse(dr["price"].ToString())
-                       }
+                    {
+                        ProdName = dr["prodname"].ToString(),
+                        Price = Double.Parse(dr["price"].ToString())
+                    }
                      );
                 }
             }
@@ -57,6 +58,42 @@ namespace mvcdemo.Controllers
                 ViewBag.Message = "";
 
             return View(model);
+        }
+
+
+        [HttpGet]
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Search(string name)
+        {
+            List<Product> prods = new List<Product>();
+            using (SqlConnection con = new SqlConnection(
+                @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=msdb;Integrated Security=True"))
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("select * from products where prodname like @pattern", con);
+                cmd.Parameters.AddWithValue("@pattern", "%" + name + "%");
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    prods.Add(
+                         new Product
+                         {
+                             ProdName = dr["prodname"].ToString(),
+                             Price = Double.Parse(dr["price"].ToString()),
+                             Catcode = dr["catcode"].ToString()
+                         }
+                     );
+                }
+            }
+
+            return PartialView("SearchResult", prods); // name and model
         }
     }
 }
---
+++ Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace mvcdemo.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string pwd)
+        {
+            if ( pwd == "admin")
+            {
+                FormsAuthentication.RedirectFromLoginPage("admin", false);
+            }
+            ViewBag.Message = "Sorry! Invalid Password!";
+            return View();
+        }
+    }
+}
