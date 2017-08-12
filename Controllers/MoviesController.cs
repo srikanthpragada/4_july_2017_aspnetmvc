71c72b8d10215df5dcfb395beef804e2a25eec35
srikanthpragada <srikanthpragada@gmail.com>
2017-08-12 10:02:36 +0530

    12-AUG

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -22,6 +22,7 @@ namespace mvcdemo.Controllers
         public ActionResult Add()
         {
             Movie m = new Movie();
+            m.ReleasedOn = DateTime.Today;
             return View(m);
         }
 
@@ -35,6 +36,9 @@ namespace mvcdemo.Controllers
                     ctx.Movies.Add(movie);
                     ctx.SaveChanges();
                     ViewBag.Message = "Movie has been added successfully!";
+                    ModelState.Clear();
+                    movie = new Movie();
+                    movie.ReleasedOn = DateTime.Today;
                 }
             }
             catch(Exception ex)
@@ -44,5 +48,95 @@ namespace mvcdemo.Controllers
             return View(movie);
 
         }
+
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var ctx = new MoviesDbContext())
+                {
+                    var movie = ctx.Movies.Find(id);
+                    if (movie == null)
+                    {
+                        ViewBag.Message = "Sorry! Movie not found!";
+                        return View("showerror");
+                    }
+                    return View(movie);
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "Sorry! Could not retrieve movie information due to error!";
+                return View("showerror");
+            }
+
+            
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Movie movie)
+        {
+            try
+            {
+                using (var ctx = new MoviesDbContext())
+                {
+                    var dbmovie = ctx.Movies.Find(id);
+                    if (dbmovie == null)
+                    {
+                        ViewBag.Message = "Sorry! Movie not found!";
+                        return View("showerror");
+                    }
+
+                    // update db object
+                    dbmovie.Title = movie.Title;
+                    dbmovie.Casting = movie.Casting;
+                    dbmovie.Rating = movie.Rating;
+                    dbmovie.Lang = movie.Lang;
+                    dbmovie.ReleasedOn = movie.ReleasedOn;
+
+                    ctx.SaveChanges();
+                    ViewBag.Message = "Movie has been updated succesfully!";
+                    return View(movie);
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "Sorry! Could not update moview due to error!";
+            }
+
+            return View(movie);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var ctx = new MoviesDbContext())
+                {
+                    var movie = ctx.Movies.Find(id);
+                    if (movie == null)
+                    {
+                        ViewBag.Message = "Sorry! Movie not found!";
+                        return View("showerror");
+                    }
+
+                    ctx.Movies.Remove(movie);
+                    ctx.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "Sorry! Could not retrieve movie information due to error!";
+                return View("showerror");
+            }
+
+
+        }
+
     }
 }
---
+++ Models/MovieUtilities.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemo.Models
+{
+    public class MovieUtilities
+    {
+        public static List<SelectListItem> GetRatings()
+        {
+            var ratings = new List<SelectListItem>();
+
+            ratings.Add(new SelectListItem { Text = "1" });
+            ratings.Add(new SelectListItem { Text = "2" });
+            ratings.Add(new SelectListItem { Text = "3" });
+            ratings.Add(new SelectListItem { Text = "4" });
+            ratings.Add(new SelectListItem { Text = "5" });
+
+            return ratings; 
+        }
+
+        public static List<SelectListItem> GetLanguages()
+        {
+            var langs = new List<SelectListItem>();
+
+            langs.Add(new SelectListItem { Text = "English", Value ="e" });
+            langs.Add(new SelectListItem { Text = "Hindi", Value = "h" });
+            langs.Add(new SelectListItem { Text = "Telugu", Value = "t" });
+            return langs;
+        }
+
+
+        public static string GetRatingString(int rating)
+        {
+            string s = "*";
+
+            for (int i = 2; i <= rating; i++)
+                s += "*";
+
+            return s; 
+        }
+
+        public static string GetLanguage(string lang)
+        {
+            switch(lang)
+            {
+                case "e": return "English";
+                case "h": return "Hindi";
+                case "t": return "Telugu";
+                default: return "Unknown";
+            }
+        }
+    }
+}
